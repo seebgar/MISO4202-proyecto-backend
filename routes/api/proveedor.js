d0952b961ba4a3d6345f1001d76901fdd24ebaff
cristian <cf.epulveda@uniandes.edu.co>
2020-04-19 15:46:20 -0500

    enable jwt

--- routes/api/proveedor.js
+++ routes/api/proveedor.js
@@ -1,10 +1,15 @@
 const ProveedorController = require("../../controllers/proveedor.controller");
+const route = require("../../config/middleware");
 
 module.exports = function (router) {
-  router.post("/proveedor", ProveedorController.create);
-  router.get("/proveedor", ProveedorController.getAll);
-  router.get("/proveedor/:id", ProveedorController.get);
-  router.get("/proveedor/insumo/:id", ProveedorController.getAllByInsumo);
-  router.put("/proveedor/:id", ProveedorController.update);
-  router.delete("/proveedor/:id", ProveedorController.delete);
+  router.post("/proveedor", route.protected, ProveedorController.create);
+  router.get("/proveedor", route.protected, ProveedorController.getAll);
+  router.get("/proveedor/:id", route.protected, ProveedorController.get);
+  router.get(
+    "/proveedor/insumo/:id",
+    route.protected,
+    ProveedorController.getAllByInsumo
+  );
+  router.put("/proveedor/:id", route.protected, ProveedorController.update);
+  router.delete("/proveedor/:id", route.protected, ProveedorController.delete);
 };

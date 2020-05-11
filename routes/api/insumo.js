90c1f343d3188cd93c9df8291355b0c01c3c43b5
cristian <cf.epulveda@uniandes.edu.co>
2020-05-10 20:12:50 -0500

    fix path

--- routes/api/insumo.js
+++ routes/api/insumo.js
@@ -2,9 +2,9 @@ const InsumoController = require("../../controllers/insumo.controller");
 const route = require("../../config/middleware");
 
 module.exports = function (router) {
-  router.post("/insumo", route.protected, InsumoController.create);
-  router.get("/insumo", route.protected, InsumoController.getAll);
-  router.get("/insumo/:id", route.protected, InsumoController.get);
-  router.put("/insumo/:id", route.protected, InsumoController.update);
-  router.delete("/insumo/:id", route.protected, InsumoController.delete);
+  router.post("/insumos", route.protected, InsumoController.create);
+  router.get("/insumos", route.protected, InsumoController.getAll);
+  router.get("/insumos/:id", route.protected, InsumoController.get);
+  router.put("/insumos/:id", route.protected, InsumoController.update);
+  router.delete("/insumos/:id", route.protected, InsumoController.delete);
 };
